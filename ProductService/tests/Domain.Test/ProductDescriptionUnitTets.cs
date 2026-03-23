1d4637f6d4f59b128efed22eb559b96d2ad592ba
mfglr <mfgglr@outlook.com>
2026-03-23 16:19:04 +0300

    test(Product): Add unit tests for ProductDescription validation

---
+++ ProductService/tests/Domain.Test/ProductDescriptionUnitTets.cs
@@ -0,0 +1,50 @@
+﻿namespace Domain.Test
+{
+    public class ProductDescriptionUnitTets
+    {
+        [Theory]
+        [InlineData("aaaaaaaaaa")]
+        [InlineData("aaaaaaaaaaa")]
+        public void ProductDescription_ShouldSetValue_WhenValid(string value)
+        {
+            var description = new ProductDescription(value);
+            Assert.Equal(value, description.Value);
+        }
+        [Fact]
+        public void ProductDescription_ShouldSetValue_WhenValueIsAtMaxLength()
+        {
+            var value = new string('a', 2000);
+
+            var description = new ProductDescription(value);
+            Assert.Equal(value, description.Value);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ProductDescription_ShouldThrowException_WhenValueIsEmptyOrNull(string? value)
+        {
+            Assert.Throws<InvalidProductDescription>(() => new ProductDescription(value));
+        }
+        [Theory]
+        [InlineData("a")]
+        [InlineData("aa")]
+        [InlineData("aaa")]
+        [InlineData("aaaa")]
+        [InlineData("aaaaa")]
+        [InlineData("aaaaaa")]
+        [InlineData("aaaaaaa")]
+        [InlineData("aaaaaaaa")]
+        [InlineData("aaaaaaaaa")]
+        public void ProductDescription_ShouldThrowException_WhenInvalid(string value)
+        {
+            Assert.Throws<InvalidProductDescription>(() => new ProductDescription(value));
+        }
+        [Fact]
+        public void ProductDescription_ShouldThrowException_WhenValueIsGreaterThanMaxLength()
+        {
+            var value = new string('a', 2001);
+            Assert.Throws<InvalidProductDescription>(() => new ProductDescription(value));
+        }
+    }
+}
