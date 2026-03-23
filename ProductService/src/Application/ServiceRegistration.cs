6f26cf7e3bc5f274bf29bcb211423beeed6f9475
mfglr <mfgglr@outlook.com>
2026-03-23 20:59:22 +0300

    feat(product): Add service registration and MediatR configuration

---
+++ ProductService/src/Application/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Application.Commands.CreateProduct;
+using Application.Commands.UpdateProduct;
+using Application.Queries.SearchProduct;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration) =>
+            services
+                .AddSingleton<CreateProductCommandMapper>()
+                .AddSingleton<UpdateProductCommandMapper>()
+                .AddSingleton<SearchProductQueryMapper>()
+                .AddMediatR(
+                    cfg => {
+                        cfg.LicenseKey = configuration.GetSection("LuckPenny:LicenseKey").Value;
+                        cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+                    }
+                )
+                .AddTransient(typeof(IPipelineBehavior<,>), typeof(UnitOfWorkPipelineBehavior<,>));
+    }
+}
