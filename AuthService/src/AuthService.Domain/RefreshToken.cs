226ab86920dc0f744c1dddaa1cf158bd9aea3766
mfglr <mfgglr@outlook.com>
2026-03-24 15:00:48 +0300

    feat(auth): Add refresh token with unit tests.

---
+++ AuthService/src/AuthService.Domain/RefreshToken.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthService.Domain
+{
+    public class RefreshToken
+    {
+        public string Token { get; private set; }
+        public byte[] TokenHash { get; private set; }
+        public DateTime ExpiredAt { get; private set; }
+
+        public RefreshToken(TimeSpan timeSpan)
+        {
+            Token = Guid.NewGuid().ToString();
+            var bytes = Encoding.UTF8.GetBytes(Token);
+            TokenHash = SHA256.HashData(bytes);
+            
+            ExpiredAt = DateTime.UtcNow.Add(timeSpan);
+        }
+
+        public bool IsValid(string token)
+        {
+            ArgumentNullException.ThrowIfNull(token,nameof(token));
+
+            var bytes = Encoding.UTF8.GetBytes(token);
+            var hash = SHA256.HashData(bytes);
+            return hash.SequenceEqual(TokenHash) && DateTime.UtcNow < ExpiredAt;
+        }
+    }
+}
---
+++ AuthService/tests/AuthService.Domain.Test/RefreshTokenUnitTests.cs
@@ -0,0 +1,46 @@
+﻿namespace AuthService.Domain.Test
+{
+    public class RefreshTokenUnitTests
+    {
+        [Fact]
+        public void IsValid_ShouldReturnTrue_WhenTokenIsCorrectAndNotExpired()
+        {
+            var refreshToken = new RefreshToken(TimeSpan.FromMinutes(5));
+            var token = refreshToken.Token;
+
+            var result = refreshToken.IsValid(token);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsValid_ShouldReturnFalse_WhenTokenIsIncorrect()
+        {
+            var refreshToken = new RefreshToken(TimeSpan.FromMinutes(5));
+            var wrongToken = Guid.NewGuid().ToString();
+
+            var result = refreshToken.IsValid(wrongToken);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValid_ShouldReturnFalse_WhenTokenIsExpired()
+        {
+            var refreshToken = new RefreshToken(TimeSpan.Zero);
+            var token = refreshToken.Token;
+
+            var result = refreshToken.IsValid(token);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsValid_ShouldThrowException_WhenTokenIsNull()
+        {
+            var refreshToken = new RefreshToken(TimeSpan.FromMinutes(5));
+
+            Assert.Throws<ArgumentNullException>(() => refreshToken.IsValid(null!));
+        }
+    }
+}
